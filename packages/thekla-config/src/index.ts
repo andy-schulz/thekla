6039f23448d43a0ced56a9fadcccb2980ea41a0b
Andy Schulz <andy.schulz@test-steps.de>
2019-10-28 11:17:39 +0100

    add WindowConfig interface to exports

--- packages/thekla-config/src/index.ts
+++ packages/thekla-config/src/index.ts
@@ -1,7 +1,13 @@
-export {TheklaGlobal}                                                             from "./lib/config/TheklaGlobal";
-export {TheklaConfig, CucumberOptions, JasmineOptions, TestFramework}             from "./lib/config/TheklaConfig";
-export {ServerConfig, AutomationFramework, LogLevel}                              from "./lib/config/ServerConfig"
-export {DesiredCapabilities, AppiumOptions, BrowserStackCapabilities, ProxyConfig}from "./lib/config/DesiredCapabilities";
-export {RestClientConfig}                                                         from "./lib/config/RestClientConfig";
-export {getConfiguredTheklaGlobal}                                                from "./lib/finder/config_finder";
-export {TheklaConfigProcessor}                                                    from "./lib/processor/TheklaConfigProcessor";
+export {TheklaGlobal}                                                   from "./lib/config/TheklaGlobal";
+export {TheklaConfig, CucumberOptions, JasmineOptions, TestFramework}   from "./lib/config/TheklaConfig";
+export {ServerConfig, ServerAddress, AutomationFramework, LogLevel}     from "./lib/config/ServerConfig"
+export {RestClientConfig}                                               from "./lib/config/RestClientConfig";
+export {getConfiguredTheklaGlobal}                                      from "./lib/finder/config_finder";
+export {TheklaConfigProcessor}                                          from "./lib/processor/TheklaConfigProcessor";
+export {transformToWdioConfig}                                          from "./lib/processor/config_transformation";
+export {ConfigurationValidationFailed}                                  from "./lib/error/ConfigurationValidationFailed";
+
+export {
+    DesiredCapabilities, AppiumOptions, BrowserStackCapabilities, ProxyConfig, WindowConfig
+}
+    from "./lib/config/DesiredCapabilities";
