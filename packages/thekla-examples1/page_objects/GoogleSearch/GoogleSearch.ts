f2413a580b993baefdaf1de0a6bafdf7689e272c
Andy Schulz <andy.schulz@test-steps.de>
2019-10-30 12:12:03 +0100

    remove examples as it will be imported as a submodule

--- packages/thekla-examples1/page_objects/GoogleSearch/GoogleSearch.ts
+++
@@ -1,6 +0,0 @@
-import {By, element, SppElement} from "../../../thekla-core/src/index";
-
-export class GoogleSearch {
-    public static searchField: SppElement = element(By.css(`[name='q']`));
-}
-
