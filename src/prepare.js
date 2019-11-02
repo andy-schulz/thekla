42c7defcf73ea18e95faa1b8bca290a153a06ca8
Andy Schulz <andy.schulz@test-steps.de>
2019-11-02 21:16:26 +0100

    bugfix CIRCLE_TAG build name

--- src/prepare.js
+++ src/prepare.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const moment = require("moment");
 
-const tagName = process.env.CIRCLE_TAG ? `${CIRCLE_TAG}_` : ``;
+const tagName = process.env.CIRCLE_TAG ? `${process.env.CIRCLE_TAG}_` : ``;
 
 const buildName = `${tagName}${moment().format(`YYYY-MM-DD_HH:mm:ss`)}`;
 
