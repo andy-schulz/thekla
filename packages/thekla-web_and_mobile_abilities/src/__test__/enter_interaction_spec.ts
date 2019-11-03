2ccd9b41ce70e4c0cabf055a39d8e5b9ea8430e1
Andy Schulz <andy.schulz@test-steps.de>
2019-11-03 18:57:53 +0100

    add new tests

---
+++ packages/thekla-web_and_mobile_abilities/src/__test__/enter_interaction_spec.ts
@@ -0,0 +1,74 @@
+import {Actor}                                                               from "@thekla/core";
+import {ServerConfig, DesiredCapabilities}                                   from "@thekla/config";
+import {getStandardTheklaServerConfig, getStandardTheklaDesiredCapabilities} from "@thekla/support";
+import {element, By, BrowseTheWeb, RunningBrowser, Enter, Navigate, Value}   from "..";
+
+describe(`Enter`, function () {
+
+    const conf: ServerConfig = getStandardTheklaServerConfig();
+    const capabilities: DesiredCapabilities = getStandardTheklaDesiredCapabilities(`enter_interaction_spec.ts`);
+
+    const emailField = element(By.css(`[data-test-id='LoginExampleRow1'] [data-test-id='exampleEmail']`));
+    const Emma = Actor.named(`Emma`);
+
+    const theBrowser = RunningBrowser.startedOn(conf).withCapabilities(capabilities);
+    Emma.whoCan(BrowseTheWeb.using(theBrowser));
+
+    beforeAll(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+    });
+
+    afterAll(() => {
+        RunningBrowser.cleanup();
+    });
+
+    describe(`a value into a field`, function () {
+
+        it(`should add the value into the input field
+        test id: 734a6687-78c8-4da7-a654-cd8086ae4388`, async () => {
+            await Navigate.to(`/`).performAs(Emma);
+
+            await Enter.value(`Enter Test`).into(emailField).performAs(Emma);
+
+            const text = await Value.of(emailField).answeredBy(Emma);
+
+            expect(text).toEqual(`Enter Test`);
+
+        });
+
+        it(`should clear the value before entering a new value
+        test id: 27e769b7-25d0-4c75-8ff6-38d0e33f05c7`, async () => {
+            await Navigate.to(`/`).performAs(Emma);
+            await Enter.value(`one`).into(emailField).performAs(Emma);
+            await Enter.value(`two`).into(emailField).performAs(Emma);
+
+            const first = await Value.of(emailField).answeredBy(Emma);
+
+            expect(first).toEqual(`onetwo`);
+
+            await Enter.value(`cleared`)
+                       .into(emailField)
+                       .butClearsTheFieldBefore()
+                       .performAs(Emma);
+
+            const second = await Value.of(emailField).answeredBy(Emma);
+
+            expect(second).toEqual(`cleared`)
+        });
+
+        it(`should not change the value if nothing is entered into the field
+        test id: b5ea22f5-12ba-4c24-86f7-0ba292e8b55b`, async () => {
+            await Navigate.to(`/`).performAs(Emma);
+
+            await Enter.value(`Enter Test`).into(emailField).performAs(Emma);
+
+            const text = await Value.of(emailField).answeredBy(Emma);
+
+            expect(text).toEqual(`Enter Test`);
+
+            await Enter.value(undefined).into(emailField).performAs(Emma);
+
+            expect(text).toEqual(`Enter Test`);
+        });
+    });
+});
--- packages/thekla-webdriver/src/__test__/unit/url_formatter_spec.ts
+++ packages/thekla-webdriver/src/__test__/unit/url_formatter_spec.ts
@@ -21,6 +21,7 @@ describe(`creating the url`, (): void => {
     });
 
     describe(`from hostname`, (): void => {
+
         it(`should return the standard selenium address 
         - (test case id: 55a16a8e-0a6e-469f-88ff-9b766a58de13)`, (): void => {
             const serverAddress: ServerAddress = {
@@ -43,6 +44,19 @@ describe(`creating the url`, (): void => {
 
             expect(getServerUrl(serverAddress2)).toEqual(`http://my.hostname.com:4444/wd/hub`);
         });
+
+        it(`should throw an Error when not a string is passed 
+        - (test case id: 9f307da2-8708-4e77-a01c-8c3c66b5776e)`, (): void => {
+
+            const serverAddress: ServerAddress = {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                // @ts-ignore to pass a number to hostname
+                hostname: 123
+            };
+
+            expect(() => getServerUrl(serverAddress)).toThrow()
+        });
+
     });
 
     describe(`from protocol`, (): void => {
@@ -65,6 +79,18 @@ describe(`creating the url`, (): void => {
 
             expect(getServerUrl(serverAddress)).toEqual(`https://my.hostname.com:443/wd/hub`)
         });
+
+        it(`should throw an error for not defined elements
+        test id: 3adb573d-84e5-4740-8f00-55c2561f075f`, function () {
+            const serverAddress: ServerAddress = {
+                hostname: `my.hostname.com`,
+                // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                // @ts-ignore to check not typed values
+                protocol: "fail" //eslint-disable-line
+            };
+
+            expect(() => getServerUrl(serverAddress)).toThrow()
+        });
     });
 
     describe(`from path`, (): void => {
@@ -76,6 +102,16 @@ describe(`creating the url`, (): void => {
             };
 
             expect(getServerUrl(serverAddress)).toEqual(`http://my.hostname.com:4444`)
+        });
+
+        it(`should return a string with host and path 
+        - (test case id: bf583ccb-ef3d-43b2-987e-ae614a4f13cf)`, (): void => {
+            const serverAddress: ServerAddress = {
+                hostname: `my.hostname.com`,
+                path: `/my/path`
+            };
+
+            expect(getServerUrl(serverAddress)).toEqual(`http://my.hostname.com:4444/my/path`)
         });
     });
 
@@ -89,6 +125,42 @@ describe(`creating the url`, (): void => {
             };
 
             expect(getServerUrl(serverAddress)).toEqual(`https://my.hostname.com:1234/wd/hub`)
+        });
+
+        it(`should throw an error when port is lower than 0 
+        - (test case id: c63a5a86-80aa-482a-b64c-9bb570cf79c2)`, (): void => {
+            const serverAddress: ServerAddress = {
+                hostname: `my.hostname.com`,
+                protocol: `https`,
+                port: -1
+            };
+
+            expect(() => getServerUrl(serverAddress)).toThrow()
+        });
+
+        it(`should throw an error when port is greater than 65535 
+        - (test case id: c63a5a86-80aa-482a-b64c-9bb570cf79c2)`, (): void => {
+            const serverAddress: ServerAddress = {
+                hostname: `my.hostname.com`,
+                protocol: `https`,
+                port: 65536
+            };
+
+            expect(() => getServerUrl(serverAddress)).toThrow()
+        });
+
+        it(`should throw an error when port is not of type number 
+        - (test case id: 91b1294e-05d6-45ac-860c-900350dc0dd4)`, (): void => {
+
+            const serverAddress: ServerAddress = {
+                hostname: `my.hostname.com`,
+                protocol: `https`,
+                // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                // @ts-ignore to pass a string as port
+                port: `1234`
+            };
+
+            expect(() => getServerUrl(serverAddress)).toThrow()
         });
     })
 });
