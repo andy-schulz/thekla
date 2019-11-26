8791600b39943f857efdd1ca1ab6ffce09636d93
Andy Schulz <andy.schulz@test-steps.de>
2019-11-26 19:49:53 +0100

    remove not used logs

--- packages/thekla-webdriver/src/__test__/support/helpers/wdio_chrome.ts
+++ packages/thekla-webdriver/src/__test__/support/helpers/wdio_chrome.ts
@@ -1,5 +1,3 @@
-process.env.FRAMEWORK = `wdio`;
 process.env.BROWSERNAME=`chrome`;
-console.log(`STARTING with FRAMEWORK:${process.env.FRAMEWORK}`);
 console.log(`STARTING with BROWSERNAME:${process.env.BROWSERNAME}`);
 
--- packages/thekla-webdriver/src/__test__/support/helpers/wdio_firefox.ts
+++ packages/thekla-webdriver/src/__test__/support/helpers/wdio_firefox.ts
@@ -1,5 +1,3 @@
-process.env.FRAMEWORK = `wdio`;
 process.env.BROWSERNAME=`firefox`;
-console.log(`STARTING with FRAMEWORK:${process.env.FRAMEWORK}`);
 console.log(`STARTING with BROWSERNAME:${process.env.BROWSERNAME}`);
 
